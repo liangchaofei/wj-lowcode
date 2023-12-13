7192c6881ede971c88d161ae09a43c3c5cc124bc
liangchaofei <liangchaofei@liangchaofeideMacBook-Pro.local>
2023-12-13 17:39:56 +0800

    feat: 布局

--- .prettierrc.js
+++ .prettierrc.js
@@ -3,10 +3,8 @@ module.exports = {
   arrowParens: 'avoid',
   // 括号内部不要出现空格
   bracketSpacing: true,
-  // 行结束符使用 Unix 格式
-  endOfLine: 'lf',
   // true: Put > on the last line instead of at a new line
-  jsxBracketSameLine: false,
+  jsxBracketSameLine: true,
   // 行宽
   printWidth: 100,
   // 换行方式
@@ -16,10 +14,9 @@ module.exports = {
   // 使用单引号
   singleQuote: false,
   // 缩进
-  tabWidth: 2,
+  tabWidth: 4,
   // 使用 tab 缩进
   useTabs: false,
   // 后置逗号，多行对象、数组在最后一行增加逗号
-  trailingComma: 'es5',
   parser: 'typescript',
 }
