2f34c82a129071a28ac781c1e740b6c7fedfc7ce
liangchaofei <liangchaofei@liangchaofeideMacBook-Pro.local>
2023-12-18 18:01:21 +0800

    feat: left panel

---
+++ src/pages/Question/Edit/Layers/index.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+
+const Layers: React.FC = () => {
+    return <div>Layers</div>;
+}
+export default Layers;
---
+++ src/pages/Question/Edit/PageSetting/index.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+
+const PageSetting = () => {
+    return <div>PageSetting</div>;
+}
+export default PageSetting;
