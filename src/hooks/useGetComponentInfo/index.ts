0838e214e83903d0ec1089e3e3e81a990c9f4ab3
liangchaofei <liangchaofei@liangchaofeideMacBook-Pro.local>
2023-12-18 18:14:58 +0800

    feat: right panel

--- src/hooks/useGetComponentInfo/index.ts
+++ src/hooks/useGetComponentInfo/index.ts
@@ -9,10 +9,11 @@ function useGetComponentInfo() {
     ) as ComponentsStateType
 
     const { componentList = [], selectedId } = components
-
+    const selectedComponent = componentList.find(c => c.fe_id === selectedId)
     return {
         componentList,
         selectedId,
+        selectedComponent
     }
 }
 
